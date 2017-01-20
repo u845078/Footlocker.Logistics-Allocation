156caeacae9d289e77f1ac9c2e7a5515946d5e2d
Evan Zimmerman <Evan.Zimmerman@footlocker.com>
2017-01-20 14:08:54 +0000

    All files created and modified for the implementation of the new purging system.
    git-tfs-id: [http://mlwweb05:8080/tfs/defaultcollection]$/Footlocker.Logistics/Allocation/Main;C24234

---
+++ Allocation/Footlocker.Logistics.Allocation/Models/InstanceModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Footlocker.Logistics.Allocation.Models
+{
+    public class InstanceModel
+    {
+        public Instance Instance { get; set; }
+        public bool Selected { get; set; }
+    }
+}
---
+++ Allocation/Footlocker.Logistics.Allocation/Models/PurgeArchiveTypeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Footlocker.Logistics.Allocation.Models
+{
+    public class PurgeArchiveTypeModel
+    {
+        public PurgeArchiveTypeModel()
+        {
+            Instances = new List<InstanceModel>();
+        }
+        public PurgeArchiveType purgeArchiveType { get; set; }
+        public List<InstanceModel> Instances { get; set; }
+    }
+}
