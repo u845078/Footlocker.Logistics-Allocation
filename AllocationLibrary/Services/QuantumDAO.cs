19c1f5477b775d9748740868b81623268ed0bb52
u773971 <u773971@corp.tfs.local>
2018-09-12 19:32:38 +0000

    Add new button for Quantum data download page, with new cloned data handling
    git-tfs-id: [http://mlwweb05:8080/tfs/defaultcollection]$/Footlocker.Logistics/Allocation/Development/Trims;C28374

--- AllocationLibrary/Services/QuantumDAO.cs
+++ AllocationLibrary/Services/QuantumDAO.cs
@@ -110,5 +110,29 @@ namespace Footlocker.Logistics.Allocation.Services
 
             return wsmList;
         }
+
+
+        public List<WSM> GetWSMextract(string sku)
+        {
+            List<WSM> wsmList = new List<WSM>();
+            string SQL = "dbo.[GetWSMextract]";
+            var sqlCommand = Footlocker.Common.DatabaseService.GetStoredProcCommand(_database, SQL);
+            _database.AddInParameter(sqlCommand, "@sku", DbType.String, sku);
+            sqlCommand.CommandTimeout = 300;
+
+            DataSet data = _database.ExecuteDataSet(sqlCommand);
+
+            WSMFactory wsmFactory = new WSMFactory();
+
+            if (data.Tables.Count > 0)
+            {
+                foreach (DataRow dr in data.Tables[0].Rows)
+                {
+                    wsmList.Add(wsmFactory.Create(dr));
+                }
+            }
+
+            return wsmList;
+        }
     }
 }
