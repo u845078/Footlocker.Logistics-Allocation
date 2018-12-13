80174759cf1a6df4936dc5d05f87b8d610fdaf5d
u773971 <u773971@corp.tfs.local>
2018-12-13 14:34:54 +0000

    updaterangeheader needs to run after db.savechanges
    git-tfs-id: [http://mlwweb05:8080/tfs/defaultcollection]$/Footlocker.Logistics/Allocation/Development/qddownload;C28972

--- Footlocker.Logistics.Allocation/Controllers/SkuRangeController.cs
+++ Footlocker.Logistics.Allocation/Controllers/SkuRangeController.cs
@@ -4114,6 +4114,7 @@ namespace Footlocker.Logistics.Allocation.Controllers
                     foreach (DeliveryGroup dg in list)
                     {
                         UpdateDeliveryGroupDates(dg);
+                        UpdateRangeHeader(dg.PlanID);
                     }
 
 
