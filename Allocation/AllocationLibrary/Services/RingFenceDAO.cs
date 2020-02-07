f6e52708ef1f384bf57f1bd211cec9cc1e39630a
Christopher Dick <Christopher.Dick@footlocker.com>
2020-02-07 15:32:59 +0000

    Reduced future inventory check batch size from 30 to 20
    git-tfs-id: [http://mlwweb05:8080/tfs/defaultcollection]$/Footlocker.Logistics/Allocation/Main;C31540

--- Allocation/AllocationLibrary/Services/RingFenceDAO.cs
+++ Allocation/AllocationLibrary/Services/RingFenceDAO.cs
@@ -111,7 +111,7 @@ namespace Footlocker.Logistics.Allocation.Models.Services
             List<WarehouseInventory> returnValue = new List<WarehouseInventory>();
             var futureSizes = uniqueCombos.Where(uc => uc.Item2.Length.Equals(3)).ToList();
             var futureCaselots = uniqueCombos.Where(uc => uc.Item2.Length.Equals(5)).ToList();
-            int batchSize = 30;
+            int batchSize = 20;
 
             if (futureSizes.Count > 0)
             {
@@ -128,7 +128,7 @@ namespace Footlocker.Logistics.Allocation.Models.Services
                 
             }
 
-            batchSize = 30;
+            batchSize = 20;
 
             if (futureCaselots.Count > 0)
             {
