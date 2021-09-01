3edbbed2bbe8b29029762dd37880d7116cc07016
Christopher Dick <Christopher.Dick@footlocker.com>
2021-09-01 13:25:20 +0000

    Fixed test project
    git-tfs-id: [http://mlwweb05:8080/tfs/defaultcollection]$/Footlocker.Logistics/Allocation/Development/AllocationUpgrades;C34044

---
+++ Allocation/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3325dfe7-386b-4d74-afda-25893c70f531")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
