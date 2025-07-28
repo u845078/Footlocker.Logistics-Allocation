9e7e9759dee34b209a0ca186da8e5f2a5c789727
Christopher Dick <Christopher.Dick@footlocker.com>
2025-07-28 20:35:54 +0000

    Added StoreDAO
    git-tfs-id: [http://mlwweb05:8080/tfs/defaultcollection]$/Footlocker.Logistics/Allocation/Main;C36629

---
+++ Allocation/AllocationLibrary/Services/StoreDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Footlocker.Logistics.Allocation.Models;
+using System.Linq;
+
+namespace Footlocker.Logistics.Allocation.Services
+{
+    public class StoreDAO
+    {
+        readonly AllocationLibraryContext db = new AllocationLibraryContext();
+
+        public ValidStoreLookup GetValidStore(string division, string storeNumber)
+        {
+            return db.ValidStores.Where(vs => vs.Division == division && vs.Store == storeNumber).FirstOrDefault();
+        }
+    }
+}
